db0ebd1bd4067d8b1c23ca7e5c2bb1cf7ab17f37
Klokov, Andrei (Andrei) **CTR** <aklokov@avaya.com>
2022-05-23 10:12:11 +0300

    Initial commit

---
+++ src/main/java/com/avaya/ccaas/call_state_builder/CallStateBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.avaya.ccaas.call_state_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallStateBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallStateBuilderApplication.class, args);
+    }
+
+}
